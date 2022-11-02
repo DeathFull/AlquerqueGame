294b094438f8a9fff115c0294848aaca0f2dfa49
DeathFull <contact@deathfull.net>
2022-11-02 10:39:00 +0100

    Passage du code en POO

--- main.py
+++ main.py
@@ -22,8 +22,24 @@ class Plateau:
     def setTableau(self, tab):
         self.tableau = tab
 
-    def switchPlayer(self, p):
+    def getCurrentPlayer(self):
+        return self.player
+
+    def switchPlayer(self):
         self.player = 2 if self.player == 1 else 2
+
+    def drawGame(self, fen):
+        fen.fill((125, 125, 125))
+        plateau_img = pygame.transform.scale(pygame.image.load("img/plateau.png"), (450, 450))
+        plateau_rect = plateau_img.get_rect(center=(325, 325))
+        fen.blit(plateau_img, plateau_rect)
+
+        for i in range(len(self.tableau)):
+            for k in range(len(self.tableau[i])):
+                self.tableau[i][k].updateImage()
+                self.tableau[i][k].updateRect((100 + (k * 112.5), 100 + (i * 112.5)))
+                if self.tableau[i][k].image is not None and self.tableau[i][k].player != 0:
+                    fen.blit(self.tableau[i][k].image, self.tableau[i][k].rect)
 
     def selectPion(self, event: pygame.event.Event):
         for i in range(len(self.tableau)):
@@ -37,6 +53,118 @@ class Plateau:
             for j in range(len(self.tableau[i])):
                 if self.tableau[i][j].player == player:
                     return True
+
+    def caseVoisins(self, pion: tuple):
+        voisins = []
+        if 0 <= pion[0] - 1 < len(self.tableau):
+            voisins.append((pion[0] - 1, pion[1]))
+        if 0 <= pion[0] + 1 < len(self.tableau):
+            voisins.append((pion[0] + 1, pion[1]))
+        if 0 <= pion[1] - 1 < len(self.tableau):
+            voisins.append((pion[0], pion[1] - 1))
+        if 0 <= pion[1] + 1 < len(self.tableau):
+            voisins.append((pion[0], pion[1] + 1))
+        if (pion[0] % 2 == 0 and pion[1] % 2 == 0) or (pion[0] % 2 != 0 and pion[1] % 2 != 0):
+            if 0 <= pion[0] - 1 < len(self.tableau) and 0 <= pion[1] - 1 < len(self.tableau):
+                voisins.append((pion[0] - 1, pion[1] - 1))
+            if 0 <= pion[0] + 1 < len(self.tableau) and 0 <= pion[1] - 1 < len(self.tableau):
+                voisins.append((pion[0] + 1, pion[1] - 1))
+            if 0 <= pion[0] - 1 < len(self.tableau[pion[0]]) and 0 <= pion[1] + 1 < len(self.tableau):
+                voisins.append((pion[0] - 1, pion[1] + 1))
+            if 0 <= pion[0] + 1 < len(self.tableau[pion[0]]) and 0 <= pion[1] + 1 < len(self.tableau):
+                voisins.append((pion[0] + 1, pion[1] + 1))
+        return voisins
+
+    def caseVoisinsCanCapture(self, pion: tuple):
+        voisins = []
+        if 0 <= pion[0] - 2 < len(self.tableau):
+            voisins.append((pion[0] - 2, pion[1]))
+        if 0 <= pion[0] + 2 < len(self.tableau):
+            voisins.append((pion[0] + 2, pion[1]))
+        if 0 <= pion[1] - 2 < len(self.tableau):
+            voisins.append((pion[0], pion[1] - 2))
+        if 0 <= pion[1] + 2 < len(self.tableau):
+            voisins.append((pion[0], pion[1] + 2))
+        if (pion[0] % 2 == 0 and pion[1] % 2 == 0) or (pion[0] % 2 != 0 and pion[1] % 2 != 0):
+            if 0 <= pion[0] - 2 < len(self.tableau) and 0 <= pion[1] - 2 < len(self.tableau):
+                voisins.append((pion[0] - 2, pion[1] - 2))
+            if 0 <= pion[0] + 2 < len(self.tableau) and 0 <= pion[1] - 2 < len(self.tableau):
+                voisins.append((pion[0] + 2, pion[1] - 2))
+            if 0 <= pion[0] - 2 < len(self.tableau[pion[0]]) and 0 <= pion[1] + 2 < len(self.tableau):
+                voisins.append((pion[0] - 2, pion[1] + 2))
+            if 0 <= pion[0] + 2 < len(self.tableau[pion[0]]) and 0 <= pion[1] + 2 < len(self.tableau):
+                voisins.append((pion[0] + 2, pion[1] + 2))
+        return voisins
+
+    def capturePion(self, pion1: tuple, pion2: tuple):
+        for items in self.caseVoisins(pion1):
+            if items in self.caseVoisins(pion2) and (
+                    self.tableau[items[0]][items[1]].getPlayer() != (self.player and 0)) and (
+                    (((pion1[0] + pion2[0]) / 2, (pion1[1] + pion2[1]) / 2) == items) or (
+                    pion1[0] == pion2[0] == items[0] or pion1[1] == pion2[1] == items[1])):
+                return items
+        return None
+
+    def verifCanCapturePion(self, pion1: tuple, pion2: tuple):
+        for items in self.caseVoisins(pion1):
+            if self.tableau[pion2[0]][pion2[1]].getPlayer() == 0 and items in self.caseVoisins(pion2) and (
+                    self.tableau[items[0]][items[1]].getPlayer() != self.player and self.tableau[items[0]][
+                items[1]].getPlayer() != 0) and ((((pion1[0] + pion2[0]) / 2, (pion1[1] + pion2[1]) / 2) == items) or (
+                    pion1[0] == pion2[0] == items[0] or pion1[1] == pion2[1] == items[1])):
+                return items
+        return None
+
+    def canCapturePion(self):
+        for i in range(len(self.tableau)):
+            for j in range(len(self.tableau[i])):
+                if self.tableau[i][j].getPlayer() == self.player:
+                    for item in self.caseVoisinsCanCapture((i, j)):
+                        if self.verifCanCapturePion((i, j), item) is not None:
+                            return True
+        return False
+
+    def canMove(self):
+        for i in range(len(self.tableau)):
+            for j in range(len(self.tableau[i])):
+                if self.tableau[i][j].getPlayer() == self.player:
+                    for item in self.caseVoisins((i, j)):
+                        if self.tableau[item[0]][item[1]].getPlayer() == 0:
+                            return True
+        return False
+
+    def movePion(self, pion1: tuple, pion2: tuple):
+        if pion2 in self.caseVoisins(pion1) and self.tableau[pion2[0]][pion2[1]].getPlayer() == 0:
+            if self.canCapturePion() is False:
+                self.tableau[pion2[0]][pion2[1]].setPlayer(self.player)
+            self.tableau[pion1[0]][pion1[1]].setPlayer(0)
+            self.switchPlayer()
+            return True
+        elif self.capturePion(pion1, pion2) is not None:
+            pioncap = self.capturePion(pion1, pion2)
+            self.tableau[pioncap[0]][pioncap[1]].setPlayer(0)
+            self.tableau[pion1[0]][pion1[1]].setPlayer(0)
+            self.tableau[pion2[0]][pion2[1]].setPlayer(self.player)
+            if self.canCapturePion() is False:
+                self.switchPlayer()
+            return True
+        else:
+            return False
+
+    def isPossibleToPlay(self, player: int):
+        if self.canMove() and self.hasPionInPlateau(player):
+            return True
+        return False
+
+    def resetPlateau(self, case, secondcase):
+        self.player = 1
+        case = None
+        secondcase = None
+        tab.setTableau([[Pion(2), Pion(2), Pion(2), Pion(2), Pion(2)],
+                        [Pion(2), Pion(2), Pion(2), Pion(2), Pion(2)],
+                        [Pion(2), Pion(2), Pion(0), Pion(1), Pion(1)],
+                        [Pion(1), Pion(1), Pion(1), Pion(1), Pion(1)],
+                        [Pion(1), Pion(1), Pion(1), Pion(1), Pion(1)]])
+        return case, secondcase
 
 
 class Pion(pygame.sprite.Sprite):
@@ -75,148 +203,11 @@ tab.setTableau([[Pion(2), Pion(2), Pion(2), Pion(2), Pion(2)],
                 [Pion(1), Pion(1), Pion(1), Pion(1), Pion(1)]])
 
 
-def drawGame():
-    fenetre.fill((125, 125, 125))
-    plateau_img = pygame.transform.scale(pygame.image.load("img/plateau.png"), (450, 450))
-    plateau_rect = plateau_img.get_rect(center=(325, 325))
-    fenetre.blit(plateau_img, plateau_rect)
-
-    for i in range(len(tableau)):
-        for k in range(len(tableau[i])):
-            tableau[i][k].updateImage()
-            tableau[i][k].updateRect((100 + (k * 112.5), 100 + (i * 112.5)))
-            if tableau[i][k].image is not None and tableau[i][k].player != 0:
-                fenetre.blit(tableau[i][k].image, tableau[i][k].rect)
-
-
-def caseVoisins(pion: tuple):
-    voisins = []
-    if 0 <= pion[0] - 1 < len(tableau):
-        voisins.append((pion[0] - 1, pion[1]))
-    if 0 <= pion[0] + 1 < len(tableau):
-        voisins.append((pion[0] + 1, pion[1]))
-    if 0 <= pion[1] - 1 < len(tableau):
-        voisins.append((pion[0], pion[1] - 1))
-    if 0 <= pion[1] + 1 < len(tableau):
-        voisins.append((pion[0], pion[1] + 1))
-    if (pion[0] % 2 == 0 and pion[1] % 2 == 0) or (pion[0] % 2 != 0 and pion[1] % 2 != 0):
-        if 0 <= pion[0] - 1 < len(tableau) and 0 <= pion[1] - 1 < len(tableau):
-            voisins.append((pion[0] - 1, pion[1] - 1))
-        if 0 <= pion[0] + 1 < len(tableau) and 0 <= pion[1] - 1 < len(tableau):
-            voisins.append((pion[0] + 1, pion[1] - 1))
-        if 0 <= pion[0] - 1 < len(tableau[pion[0]]) and 0 <= pion[1] + 1 < len(tableau):
-            voisins.append((pion[0] - 1, pion[1] + 1))
-        if 0 <= pion[0] + 1 < len(tableau[pion[0]]) and 0 <= pion[1] + 1 < len(tableau):
-            voisins.append((pion[0] + 1, pion[1] + 1))
-    return voisins
-
-
-def caseVoisinsCanCapture(pion: tuple):
-    voisins = []
-    if 0 <= pion[0] - 2 < len(tableau):
-        voisins.append((pion[0] - 2, pion[1]))
-    if 0 <= pion[0] + 2 < len(tableau):
-        voisins.append((pion[0] + 2, pion[1]))
-    if 0 <= pion[1] - 2 < len(tableau):
-        voisins.append((pion[0], pion[1] - 2))
-    if 0 <= pion[1] + 2 < len(tableau):
-        voisins.append((pion[0], pion[1] + 2))
-    if (pion[0] % 2 == 0 and pion[1] % 2 == 0) or (pion[0] % 2 != 0 and pion[1] % 2 != 0):
-        if 0 <= pion[0] - 2 < len(tableau) and 0 <= pion[1] - 2 < len(tableau):
-            voisins.append((pion[0] - 2, pion[1] - 2))
-        if 0 <= pion[0] + 2 < len(tableau) and 0 <= pion[1] - 2 < len(tableau):
-            voisins.append((pion[0] + 2, pion[1] - 2))
-        if 0 <= pion[0] - 2 < len(tableau[pion[0]]) and 0 <= pion[1] + 2 < len(tableau):
-            voisins.append((pion[0] - 2, pion[1] + 2))
-        if 0 <= pion[0] + 2 < len(tableau[pion[0]]) and 0 <= pion[1] + 2 < len(tableau):
-            voisins.append((pion[0] + 2, pion[1] + 2))
-    return voisins
-
-
-def capturePion(pion1: tuple, pion2: tuple):
-    for items in caseVoisins(pion1):
-        if items in caseVoisins(pion2) and (
-                tableau[items[0]][items[1]].getPlayer() != (game_player and 0)) and (
-                (((pion1[0] + pion2[0]) / 2, (pion1[1] + pion2[1]) / 2) == items) or (
-                pion1[0] == pion2[0] == items[0] or pion1[1] == pion2[1] == items[1])):
-            return items
-    return None
-
-
-def verifCanCapturePion(pion1: tuple, pion2: tuple):
-    for items in caseVoisins(pion1):
-        if tableau[pion2[0]][pion2[1]].getPlayer() == 0 and items in caseVoisins(pion2) and (
-                tableau[items[0]][items[1]].getPlayer() != game_player and tableau[items[0]][
-            items[1]].getPlayer() != 0) and ((((pion1[0] + pion2[0]) / 2, (pion1[1] + pion2[1]) / 2) == items) or (
-                pion1[0] == pion2[0] == items[0] or pion1[1] == pion2[1] == items[1])):
-            return items
-    return None
-
-
-def canCapturePion():
-    for i in range(len(tableau)):
-        for j in range(len(tableau[i])):
-            if tableau[i][j].getPlayer() == game_player:
-                for item in caseVoisinsCanCapture((i, j)):
-                    if verifCanCapturePion((i, j), item) is not None:
-                        return True
-    return False
-
-
-def movePion(pion1: tuple, pion2: tuple):
-    global game_player, menu
-    if pion2 in caseVoisins(pion1) and tableau[pion2[0]][pion2[1]].getPlayer() == 0:
-        if canCapturePion() is False:
-            tableau[pion2[0]][pion2[1]].setPlayer(game_player)
-        tableau[pion1[0]][pion1[1]].setPlayer(0)
-        game_player = 1 if game_player == 2 else 2
-        return True
-    elif capturePion(pion1, pion2) is not None:
-        pioncap = capturePion(pion1, pion2)
-        tableau[pioncap[0]][pioncap[1]].setPlayer(0)
-        tableau[pion1[0]][pion1[1]].setPlayer(0)
-        tableau[pion2[0]][pion2[1]].setPlayer(game_player)
-        if canCapturePion() is False:
-            game_player = 1 if game_player == 2 else 2
-        return True
-    else:
-        return False
-
-
-
-
-def canMove():
-    for i in range(len(tableau)):
-        for j in range(len(tableau[i])):
-            if tableau[i][j].getPlayer() == game_player:
-                for item in caseVoisins((i, j)):
-                    if tableau[item[0]][item[1]].getPlayer() == 0:
-                        return True
-    return False
-
-
-def isPossibleToPlay(player: int):
-    if canMove() and hasPionInPlateau(player):
-        return True
-    return False
-
-
-def resetGame():
-    global game_player, game_case, second_game_case
-    game_player = 1
-    game_case = None
-    second_game_case = None
-    tab.setTableau([[Pion(2), Pion(2), Pion(2), Pion(2), Pion(2)],
-                    [Pion(2), Pion(2), Pion(2), Pion(2), Pion(2)],
-                    [Pion(2), Pion(2), Pion(0), Pion(1), Pion(1)],
-                    [Pion(1), Pion(1), Pion(1), Pion(1), Pion(1)],
-                    [Pion(1), Pion(1), Pion(1), Pion(1), Pion(1)]])
 
 
 app = 1
 menu = "main"
 menuhover = None
-game_player = 1
 game_case, second_game_case = None, None
 while app:
     if menu == "main":
@@ -226,7 +217,7 @@ while app:
         elif menuhover == "quit":
             fenetre.blit(pygame.image.load("img/quit.png"), (218, 431))
     elif menu == "game":
-        drawGame()
+        tab.drawGame(fenetre)
     elif menu == "end":
         fenetre.blit(pygame.image.load("img/end.png"), (0, 0))
         if menuhover == "replay":
@@ -257,23 +248,23 @@ while app:
                 if 105 <= evt.pos[0] <= 541 and 324 <= evt.pos[1] <= 541:
                     menu = "game"
             elif menu == "game":
-                click = selectPion(evt)
+                click = tab.selectPion(evt)
                 if click is None:
                     break
 
-                if game_case is None and tableau[click[0]][click[1]].getPlayer() == game_player:
+                if game_case is None and tab.getTableau()[click[0]][click[1]].getPlayer() == tab.getCurrentPlayer():
                     game_case = click
                 elif game_case is not None and second_game_case is None and \
-                        tableau[click[0]][click[1]].getPlayer() == 0 and game_case is not click:
+                        tab.getTableau()[click[0]][click[1]].getPlayer() == 0 and game_case is not click:
                     second_game_case = click
                 if game_case is None or second_game_case is None:
                     break
-                if movePion(game_case, second_game_case) is True:
+                if tab.movePion(game_case, second_game_case) is True:
                     game_case, second_game_case = None, None
-                    if canCapturePion():
+                    if tab.canCapturePion():
                         break
 
-                if isPossibleToPlay(1) is False and isPossibleToPlay(2) is False:
-                    resetGame()
+                if tab.isPossibleToPlay(1) is False and tab.isPossibleToPlay(2) is False:
+                    game_case, second_game_case = tab.resetPlateau(game_case, second_game_case)
                     menu = "end"
 pygame.quit()
